2821eed4e825a3e6795a4aea402bf9ebc4229d9c
VanRar <vitalikf7@gmail.com>
2020-06-27 19:52:22 +0300

    Добавьте файлы проекта.

---
+++ TestGIT2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGIT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("testgit2 zalivaem master vetku");
+        }
+    }
+}
